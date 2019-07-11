b5d67f79a142bb0038d5ba64473504026b6ac829
s1260235 <s1260235@u-aizu.ac.jp>
2019-07-11 16:23:03 +0900

    Won/Lost Message Added

--- DiceGame.c
+++ DiceGame.c
@@ -6,11 +6,13 @@ int main(){
   int one,two;
   int total;
   printf("Rolling the dice...\n");
-   one=rand()%6+1;
-   two=rand()%6+1;
+  one=rand()%6+1;
+  two=rand()%6+1;
   printf("Die 1: %d\n",one);
   printf("Die 2: %d\n",two);
   total=one+two;
   printf("Total value: %d\n",total);
+  if(total>7) printf("You won\n");
+  else printf("You lost\n");
   return 0;
 }
