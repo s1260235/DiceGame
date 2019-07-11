7ea56b6a8c7ba21e8a7f1e9efe619252f9956c8b
s1260235 <s1260235@u-aizu.ac.jp>
2019-07-11 16:31:23 +0900

    Added Repdigit

--- DiceGame.c
+++ DiceGame.c
@@ -3,7 +3,7 @@
 #include<time.h>
 int main(){
   srand((unsigned int)time(NULL));
-  int one,two;
+  int one,two,three;
   int total;
   char name[10];
   printf("What is your name?\n");
@@ -12,12 +12,15 @@ int main(){
   printf("Rolling the dice...\n");
   one=rand()%6+1;
   two=rand()%6+1;
+  three=rand()%6+1;
   printf("Die 1: %d\n",one);
   printf("Die 2: %d\n",two);
-  total=one+two;
+  printf("Die 3: %d\n",three);
+  total=one+two+three;
   printf("Total value: %d\n",total);
   if(total>7) printf("%s won\n",name);
   else printf("%s lost\n",name);
+  if(one==two && two==three) printf("At luck would have it, Repdigit\n");
   return 0;
 }
 
